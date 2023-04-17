281a62107c2f63914b2133491a327c990c611ba1
Navarro <antonio.navarro@iesnervion.es>
2023-04-17 10:31:06 +0200

    Falta Lavadora y Televisión

---
+++ src/ejercicios/ejercicio4/Lavadora.java
@@ -0,0 +1,21 @@
+package ejercicios.ejercicio4;
+
+/**
+ * Clase que crea una lavadora, hereda de Electrodomestico
+ * @author Antonio Navarro
+ * @version 1.0 Release
+ * @see Electrodomestico
+ */
+public class Lavadora extends Electrodomestico {
+    /**
+     * Carga que puede soportar la lavadora
+     */
+    protected float carga;
+
+    /**
+     * Constructor por Defecto
+     */
+    public Lavadora () {
+        super();
+    }
+}
