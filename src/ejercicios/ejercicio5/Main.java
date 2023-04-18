6b173428c44ae0eaad10e3a077e2da0ac6334bbf
Navarro <antonio.navarro@iesnervion.es>
2023-04-18 12:51:17 +0200

    Sistemas

---
+++ src/ejercicios/ejercicio5/Main.java
@@ -0,0 +1,8 @@
+package ejercicios.ejercicio5;
+
+public class Main {
+    public static void main(String[] args) {
+        // ^ Definimos variables copias para las clases
+        double lado1, lado2, lado3;
+    }
+}
